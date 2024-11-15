441100e39c5d86267bb452fa9cee3764845c512e
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-11-14 21:27:46 -0500

    add song button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
     <div className="container flex flex-1 flex-col gap-8 p-4 md:p-8">
-      <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-4 md:gap-8">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           BEST SONG NO ONE KNOWS ABOUT?
         </h1>
+        <Button>Add Song</Button>
       </div>
     </div>
   );
