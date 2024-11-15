529d3879daeb189359680170cc947979b8ff4406
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-11-14 21:23:02 -0500

    tw config fix, title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-  return <>hi</>;
+  return (
+    <div className="container flex flex-1 flex-col gap-8 p-4 md:p-8">
+      <div className="flex items-center justify-center">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          BEST SONG NO ONE KNOWS ABOUT?
+        </h1>
+      </div>
+    </div>
+  );
 }
